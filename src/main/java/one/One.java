961dd67e79111ed4bb7bc5816ac40108520f04e2
Bae Ji Won <qowhxk@naver.com>
2023-03-02 23:31:07 +0900

    Stack - 올바른 괄호 구분

---
+++ src/main/java/one/One.java
@@ -0,0 +1,31 @@
+package one;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+// 올바른 괄호
+public class One {
+    public String solution(String str){
+        Stack<Character> stack = new Stack<>();
+        for(char x : str.toCharArray()){
+            if(x=='(')
+                stack.push(x);
+            else
+                if(stack.isEmpty())
+                    return "NO";
+                else
+                    stack.pop();
+        }
+        if(!stack.isEmpty())
+            return "NO";
+        return "YES";
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+        One one = new One();
+        System.out.println(one.solution(str));
+    }
+}
